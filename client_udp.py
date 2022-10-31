14e4515c9947bfe416b8a7017ecf7034149c73a5
Julien Boriasse <julien@boriasse.com>
2022-10-31 14:57:46 -0400

    Description issue

--- client_udp.py
+++ client_udp.py
@@ -6,11 +6,9 @@ import sys
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Connect the socket to the port where the server is listening
-server_address = ('localhost', 10000)
-print('Connexion UDP a %s sur le port %s' % server_address)
- 
 # Send data
+server_address = ('localhost', 10000)
+print('Envoi UDP a %s sur le port %s' % server_address)
 message = 'Ceci est un messsage. Il va etre repete par le serveur.'
 print('Envoi : "%s"' % message)
 sock.sendto(message.encode(), server_address)
