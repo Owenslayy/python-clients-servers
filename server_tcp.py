eade262b187850527f22218455e9db5f9e34baa4
Julien Boriasse <julien@boriasse.com>
2025-10-20 13:46:42 -0400

    Ctrl + C pour quitter

--- server_tcp.py
+++ server_tcp.py
@@ -13,26 +13,43 @@ sock.bind(server_address)
 
 # Listen for incoming connections
 sock.listen(1)
+sock.settimeout(1.0)
 
-while True:
-    # Wait for a connection
-    print('')
-    print('Attente de connexion TCP...')
-    connection, client_address = sock.accept()
-    try:
-        print('Connexion de ', client_address)
+print('')
+print('Attente de connexion TCP...')
 
-        # Receive the data in small chunks and retransmit it
-        while True:
-            data = connection.recv(255)
-            print('Recu : "%s"' % data.decode())
-            if data:
-                print('Renvoi les memes donnees au client')
-                connection.sendall(data)
-            else:
-                print('Plus de donnees de la part de ', client_address)
-                break
-            
-    finally:
-        # Clean up the connection
-        connection.close()
+try:
+    while True:
+        # Wait for a connection
+        
+        try:
+            connection, client_address = sock.accept()
+        except socket.timeout:
+            continue
+
+        connection.settimeout(1.0)
+        try:
+            print('Connexion de ', client_address)
+
+            # Receive the data in small chunks and retransmit it
+            while True:
+                data = connection.recv(255)
+                print('Recu : "%s"' % data.decode())
+                if data:
+                    print('Renvoi les memes donnees au client')
+                    connection.sendall(data)
+                else:
+                    print('Plus de donnees de la part de ', client_address)
+                    break
+                
+        finally:
+            # Clean up the connection
+            connection.close()
+            print('')
+            print('Attente d\'une prochaine connexion TCP...')
+
+except KeyboardInterrupt:
+    print('\nArret du serveur via Ctrl+C.')
+finally:
+    sock.close()
+    print('Socket ferme.')
