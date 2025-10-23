ddb299027b95afeb8767ac7feca351797c0c0659
Owenslayy <e2288053@cmaisonneuve.qc.ca>
2025-10-23 12:11:53 -0400

    init

--- client_tcp.py
+++ client_tcp.py
@@ -7,7 +7,7 @@ import sys
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-server_address = ('localhost', 10000)
+server_address = ('10.20.61.14', 10000)
 print('Connexion TCP a %s sur le port %s' % server_address)
 sock.connect(server_address)
 
@@ -18,14 +18,6 @@ try:
     print('Envoi de : "%s"' % message)
     sock.sendall(message.encode())
     
-    # Look for the response
-    amount_received = 0
-    amount_expected = len(message)
-    
-    while amount_received < amount_expected:
-        data = sock.recv(255)
-        amount_received += len(data)
-        print('Recu :  "%s"' % data.decode())
 
 finally:
     print('Fermeture du socket TCP')
